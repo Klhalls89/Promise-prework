d6677e92db73bdd88a8328427b7026bd1dd2b2fa
Kristen Hallstrom <klhalls89@gmail.com>
2019-06-22 15:07:30 -0600

    finish promise practice

--- App.js
+++ App.js
@@ -1,18 +1,43 @@
 function testNum(num) {
-  let p = new Promise(function(resolve, reject) {
+  let promise = new Promise(function(resolve, reject) {
     if(num > 10) {
       resolve('Success!');
     } else {
       reject('Failure!');
     }
   });
-  p.then(function(result) { 
-    console.log(result)
-  }).catch(function() {
-    console.log('error')
-  }).finally(function() {
-    console.log('ran function')
-  });
+  promise.then(result => console.log(result))
+  .catch(error => console.log(error))
 }
 
 testNum(12)
+
+function sortWords(words) {
+  words = words.sort()
+  console.log('after sort', words)
+}
+
+function makeAllCaps(words) {
+
+  let promise = new Promise(function(resolve, reject) {
+
+    words = words.map(word => {
+      if(typeof(word) === 'string') {
+        resolve('Success!');
+        return word.toUpperCase();
+      } else {
+      reject('Failure!');
+      console.log('No, the array you passed in contained an element that was not a string!')
+      }
+    })
+  });
+
+  promise.then(data => sortWords(words))
+  promise.then(result => console.log(result))
+  .catch(error => console.log(error))
+
+}
+
+makeAllCaps(['wowow', 'pants', 'bird'])
+
+
